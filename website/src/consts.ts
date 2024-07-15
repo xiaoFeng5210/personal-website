12693efc125ef93cc5feba4e7efccde774e02b28
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-15 10:55:42 +0800

    finish

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '清风居';
+export const SITE_DESCRIPTION = '欢迎来到我的世界！';
