b7d332f1a5ae5c736793adaba26c986344436423
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-15 10:18:52 +0800

    init

--- website/src/config/theme.ts
+++
@@ -1,3 +0,0 @@
-export const nodeTheme = {
-	mainColor: ['#60D2D6', '#BF85D5', '#477FA6', '#3C3C43', '#EF7234']
-}
--- website/src/types/mapData.ts
+++
@@ -1,9 +0,0 @@
-export interface YakMapNode {
-	id: number;
-	x: number;
-	y: number;
-	vx: number;
-	vy: number;
-	color: string;
-	text: string;
-}
