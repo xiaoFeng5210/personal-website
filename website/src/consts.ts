53510fffb81266f46d51817a8d8821103d7e16c3
Eadweard <aurora.coder.zqf@gmail.com>
2024-07-15 11:21:57 +0800

    init

--- website/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = '清风居';
-export const SITE_DESCRIPTION = '欢迎来到我的世界！';
